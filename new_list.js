e0a8b7d1adea0f20f0e5724a9ed5195eb64de61f
Gabe Foster <mrginch@gmail.com>
2018-12-05 15:46:02 -0500

    input reset and scroll
    inputs now reset after info is made into new list
    each part of the new list is contained in a div that scrolls once it
    gets too big

--- new_list.js
+++ new_list.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 
-	function newItem(i, v, tableStr, num){
+	function newItem(i, tableStr, num){
+	       var v = [];
+
 	       v[0] = {'name': $(i[0]).children('input')[0].value};
 	       v[1] = {'weight': $(i[1]).children('input')[0].value};
-	       //values[2] = {'purchased': $(inclusiveSibs[2]).children('input')[0].value};
 
 	      if(document.getElementsByClassName('yes')[num].checked){
 	      		v[2] = {'purchased': $(i[2]).children('input')[0].value};
@@ -21,34 +22,42 @@ $(document).ready(function(){
 				+ '</td><td>' + v[3].price 
 				+ '</td><td>' + v[4].comments 
 				+ '</td></tr>').insertBefore(tableStr +'InputRow');
-	
-	 }
 
-	
+	 		resetFields(i);
+
+	 		
+	 		
+	}
+
+	function resetFields(i){
+	  $(i[0]).children('input')[0].value = 'New Item Name';
+      $(i[1]).children('input')[0].value = '0';
+      $(i[2]).children('input')[0].checked = 'checked';
+      $(i[3]).children('input')[0].value = '0';
+      $(i[4]).children('input')[0].value = '';
+	}
+
+
 	$('#baseweightTable > tbody > tr:last-child > td > input').on('keyup', function (e) {
 	    if (e.keyCode == 13) {
 	       var BWSibs = $(this).parent().parent().children();
 	       var BWvalues = [];
-	       newItem(BWSibs, BWvalues, '#baseweightTable', 0);
-    
-	    }
+	       newItem(BWSibs, '#baseweightTable', 0); 
+
+	     }
 	});
 
 	$('#wornTable > tbody > tr:last-child > td > input').on('keyup', function (e) {
 	    if (e.keyCode == 13) {
 	       var WSibs = $(this).parent().parent().children();
-	       var Wvalues = [];
-	       newItem(WSibs, Wvalues, '#wornTable', 1);
-    
+	       newItem(WSibs, '#wornTable', 1);
 	    }
 	});
 
 	$('#consumablesTable > tbody > tr:last-child > td > input').on('keyup', function (e) {
 	    if (e.keyCode == 13) {
 	       var CSibs = $(this).parent().parent().children();
-	       var Cvalues = [];
-	       newItem(CSibs, Cvalues, '#consumablesTable', 2);
-    
+	       newItem(CSibs, '#consumablesTable', 2);
 	    }
 	});
 	 
